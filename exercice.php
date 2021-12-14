4d4d24893916765b4bca21a93efff03bdfd19db3
madiou <abarry291@gmail.com>
2021-12-14 11:22:25 +0100

    first commit

---
+++ exercice.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     
+    <link rel="stylesheet" href="/css/bootstrap.css">
+    <title>Les Exercices</title>
+    
+</head>
+
+
+<body>
+<?php include("./partial/_navBar.php"); ?>
+    <div class="container">
+    <h1>Page Exercice</h1>
+    <p>les exercices en php</p>
+    </div>
+    <script src="/js/bootstrap.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="/css/bootstrap.css">
+    <title>Premier Exercice</title>
+</head>
+
+
+<body>
+<?php include("./partial/_navBar.php"); ?>
+    <div class="container">
+
+
+    <h1>Le site de gizeh</h1>
+    
+
+
+
+    
+    </div>
+
+    <script src="/js/bootstrap.js"></script>
+</body>
+</html>
