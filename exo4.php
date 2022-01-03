b28765db8285e661fd05d93de0c85cd1bcb0033e
madiou <abarry291@gmail.com>
2022-01-03 10:22:42 +0100

    exo4 exercice effectué

---
+++ exo4.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="/css/bootstrap.css">
+
+    <title>Exercice 4</title>
+</head>
+
+
+<body>
+<?php include("./partial/_navBar.php"); ?>
+    <div class="container">
+       <h1>Exercice 4</h1>
+    
+       <h5>1- créer une <a href="https://www.latoilescoute.net/table-de-vigenere" target="_blank">table de vigenère</a></h5>
+       <?php
+       // TO DO
+     
+
+
+
+
+
+       ?>
+       <h5>2- encode le message "APPRENDRE PHP EST UNE CHOSE FORMIDABLE" avec la clé "BACKEND"</h5>
+       <?php
+       $message = "APPRENDRE PHP EST UNE CHOSE FORMIDABLE";
+       $key = "BACKEND";
+       // TO DO
+       $cryptedMessage = $message;
+       /**
+      * astuce pour la rotation de la clé BACKEND
+      * 14 / 7 -> 2
+      * 15 / 7 -> 2 reste 1
+      * pour récuperer le "reste 1" il faut faire un modulo
+      * 15 % 7 -> 1
+      * 176 % 7 -> 1 (25 x 7 et reste 1)
+      */
+      ?>
+      <p>le message est: <?php echo $message; ?></p>
+      <p>la clé est: <?php echo $key ?></p>
+      <p>le résultat est: <?php echo $cryptedMessage; ?></p>
+      <h5>3- decoder le message "TWA PEE WM TESLH WL LSLVNMRJ" avec la clé "VIGENERE"</h5>
+      <?php
+      $encodedMessage = "TWA PEE WM TESLH WL LSLVNMRJ";
+      $key4decode = "VIGENERE";
+      // TO DO
+      $decodedMessage = $encodedMessage;
+      ?>
+      <p>le message chiffré est: <?php echo $encodedMessage; ?></p>
+      <p>la clé est: <?php echo $key4decode ?></p>
+      <p>le résultat est: <?php echo $decodedMessage; ?></p>
+
+
+
+    
+    </div>
+
+    <script src="/js/bootstrap.js"></script>
+</body>
+</html>
--- test2.php
+++ test2.php
@@ -33,15 +33,33 @@ RESULTATS PHP
 // $tab1 = ["moteur", "carotte", "haricot", "pomme de terre", "usine", "salade", "navet", "marteau"];
 
 
-$tab = ["1", "2", "3", "4", "5"];
-array_splice($tab, 2, 1);
+// $tab = ["1", "2", "3", "4", "5"];
+// array_splice($tab, 2, 1);
 // array_splice($tab1, 4, 1);
 // array_slice($tab1, 4, 1);
 
 // $output = array_splice($tab, 2, 1);
 // $output = array_slice($tab, 2, 1);
 // var_dump($output);
-var_dump($tab);
+// var_dump($tab);
+
+// pour table de vigenere
+$alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// $alphabet = "ABC";
+$alphabetTab = str_split($alphabet);
+$doubleAlphabetTab = array_merge($alphabetTab, $alphabetTab);
+// var_dump($doubleAlphabetTab);
+// count = permet de compter nbre de vlrs ds 1 tblo !
+$sizeAlphabet = count($alphabetTab);
+
+for ($i =0; $i < $sizeAlphabet; $i++) {
+    for($j = 0; $j < $sizeAlphabet; $j++) {
+        $line = $alphabetTab[$i];
+        $column = $alphabetTab[$j];
+        $vigenere[$line][$column] = $doubleAlphabetTab[$i + $j];
+    }
+}
+var_dump($vigenere);
 
 
 
